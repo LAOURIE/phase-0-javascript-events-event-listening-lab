2b892d0a824bbfa6d47d7f784cde677a0370e179
Grace Wambui Njau <gracenjau2005@gmail.com>
2024-04-06 09:49:18 +0300

    phase-0-javascript-events-event-listening-lab

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-function addingEventListener() {
-}
+
+    
+    
+    function addingEventListener() {
+      const button = document.getElementById('button');
+      button.addEventListener('click', () => {
+        // do something when the button is clicked
+      });
+    }
+
